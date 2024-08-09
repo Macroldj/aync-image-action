324df6b87e4fc8277e9b77f27ff3924c4991ac2d
DurginLi <dejinx@qq.com>
2024-08-09 17:26:26 +0800

    ci(init):sync images

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 # docker pull
-docker pull nginx:1.28.0
+docker pull nginx:1.27.0
 # docker tag
-docker tag nginx:1.28.0 registry.cn-hangzhou.aliyuncs.com/macroldj/nginx:1.28.0
+docker tag nginx:1.27.0 registry.cn-hangzhou.aliyuncs.com/macroldj/nginx:1.27.0
 # docker push
-docker push registry.cn-hangzhou.aliyuncs.com/macroldj/nginx:1.28.0
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/nginx:1.27.0
