bfa7e7e2a852f9c716cbbc66709fbf50085c5aea
DurginLi <dejinx@qq.com>
2024-08-16 16:12:56 +0800

    ci(init): docker.elastic.co/kibana/kibana:8.15.0 sync

--- sync.sh
+++ sync.sh
@@ -10,7 +10,11 @@ done
 #docker pull seafileltd/seafile-mc:11.0-latest
 #docker tag seafileltd/seafile-mc:11.0-latest registry.cn-hangzhou.aliyuncs.com/macroldj/seafile-mc:11.0-latest
 #docker push registry.cn-hangzhou.aliyuncs.com/macroldj/seafile-mc:11.0-latest
+#
+#docker pull docker.elastic.co/elasticsearch/elasticsearch:8.15.0
+#docker tag docker.elastic.co/elasticsearch/elasticsearch:8.15.0 registry.cn-hangzhou.aliyuncs.com/macroldj/elasticsearch:8.15.0
+#docker push registry.cn-hangzhou.aliyuncs.com/macroldj/elasticsearch:8.15.0
 
-docker pull docker.elastic.co/elasticsearch/elasticsearch:8.15.0
-docker tag docker.elastic.co/elasticsearch/elasticsearch:8.15.0 registry.cn-hangzhou.aliyuncs.com/macroldj/elasticsearch:8.15.0
-docker push registry.cn-hangzhou.aliyuncs.com/macroldj/elasticsearch:8.15.0
+docker pull docker.elastic.co/kibana/kibana:8.15.0
+docker tag docker.elastic.co/kibana/kibana:8.15.0 registry.cn-hangzhou.aliyuncs.com/macroldj/kibana:8.15.0
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/kibana:8.15.0
