328cf867bfe3748d5008672b6d0b36bf90aad33b
Durgin.Li <dejinx@qq.com>
2024-08-26 14:36:18 +0800

    ci(init): sync apacherocketmq/rocketmq:4.5.0 services

--- sync.sh
+++ sync.sh
@@ -34,3 +34,7 @@ done
 docker pull mongodb/mongodb-community-server:6.0.7-ubuntu2204
 docker tag mongodb/mongodb-community-server:6.0.7-ubuntu2204 registry.cn-hangzhou.aliyuncs.com/macroldj/mongodb-community-server:6.0.7-ubuntu2204
 docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mongodb-community-server:6.0.7-ubuntu2204
+
+docker pull apacherocketmq/rocketmq:4.5.0
+docker tag apacherocketmq/rocketmq:4.5.0 registry.cn-hangzhou.aliyuncs.com/macroldj/rocketmq:4.5.0
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/rocketmq:4.5.0
