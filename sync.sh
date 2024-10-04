02c9f7d4d95db8e5f9b1685a989cfe2a690b9c3f
Durgin.Li <dejinx@qq.com>
2024-10-04 08:07:16 +0800

    ci(init): rancher/fleet-agent:v0.10.2

--- sync.sh
+++ sync.sh
@@ -213,6 +213,15 @@ done
 #docker tag rancher/mirrored-kube-logging-logging-operator:4.8.0 registry.cn-hangzhou.aliyuncs.com/macroldj/kube-logging-logging-operator:4.8.0
 #docker push registry.cn-hangzhou.aliyuncs.com/macroldj/kube-logging-logging-operator:4.8.0
 
-docker pull rancher/mirrored-kube-logging-fluentd:v1.16-4.8-full
-docker tag rancher/mirrored-kube-logging-fluentd:v1.16-4.8-full registry.cn-hangzhou.aliyuncs.com/macroldj/kube-logging-fluentd:v1.16-4.8-full
-docker push registry.cn-hangzhou.aliyuncs.com/macroldj/kube-logging-fluentd:v1.16-4.8-full
+#docker pull rancher/mirrored-kube-logging-fluentd:v1.16-4.8-full
+#docker tag rancher/mirrored-kube-logging-fluentd:v1.16-4.8-full registry.cn-hangzhou.aliyuncs.com/macroldj/kube-logging-fluentd:v1.16-4.8-full
+#docker push registry.cn-hangzhou.aliyuncs.com/macroldj/kube-logging-fluentd:v1.16-4.8-full
+
+docker pull rancher/mirrored-fluent-fluent-bit:2.2.0
+docker tag rancher/mirrored-fluent-fluent-bit:2.2.0 registry.cn-hangzhou.aliyuncs.com/macroldj/fluent-fluent-bit:2.2.0
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/fluent-fluent-bit:2.2.0
+
+docker pull rancher/mirrored-jimmidyson-configmap-reload:v0.4.0
+docker tag rancher/mirrored-jimmidyson-configmap-reload:v0.4.0 registry.cn-hangzhou.aliyuncs.com/macroldj/jimmidyson-configmap-reload:v0.4.0
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/jimmidyson-configmap-reload:v0.4.0
+
