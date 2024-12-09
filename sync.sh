ff590a3245f2f05743647a2b944edf037ece4fca
Durgin.Li <dejinx@qq.com>
2024-12-09 17:50:02 +0800

    ci(init): ubuntu:24.04

--- sync.sh
+++ sync.sh
@@ -387,6 +387,10 @@ done
 #docker tag golang:1.22.10-alpine3.21 registry.cn-hangzhou.aliyuncs.com/macroldj/golang:1.22.10-alpine3.21
 #docker push registry.cn-hangzhou.aliyuncs.com/macroldj/golang:1.22.10-alpine3.21
 
-docker pull alpine:3.21.0
-docker tag alpine:3.21.0 registry.cn-hangzhou.aliyuncs.com/macroldj/alpine:3.21.0
-docker push registry.cn-hangzhou.aliyuncs.com/macroldj/alpine:3.21.0
+#docker pull alpine:3.21.0
+#docker tag alpine:3.21.0 registry.cn-hangzhou.aliyuncs.com/macroldj/alpine:3.21.0
+#docker push registry.cn-hangzhou.aliyuncs.com/macroldj/alpine:3.21.0
+
+docker pull ubuntu:24.04
+docker tag ubuntu:24.04 registry.cn-hangzhou.aliyuncs.com/macroldj/ubuntu:24.04
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/ubuntu:24.04
