d824c2658f4a2d6771590fab05dfd9c608eea378
Durgin.Li <dejinx@qq.com>
2025-02-10 11:03:18 +0800

    build(sync.sh): 更新 CentOS镜像
    - 在 sync.sh 脚本中添加了 CentOS 8 和 CentOS 7镜像的同步操作
    -从 Docker Hub 拉取 CentOS 官方镜像，并推送到阿里云容器镜像服务

--- sync.sh
+++ sync.sh
@@ -470,3 +470,11 @@ docker push registry.cn-hangzhou.aliyuncs.com/macroldj/docker:20.10.17
 docker pull jenkins/inbound-agent:3206.vb_15dcf73f6a_9-2
 docker tag jenkins/inbound-agent:3206.vb_15dcf73f6a_9-2 registry.cn-hangzhou.aliyuncs.com/macroldj/inbound-agent:3206.vb_15dcf73f6a_9-2
 docker push registry.cn-hangzhou.aliyuncs.com/macroldj/inbound-agent:3206.vb_15dcf73f6a_9-2
+
+docker pull centos:8
+docker tag centos:8 registry.cn-hangzhou.aliyuncs.com/macroldj/centos:8
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/centos:8
+
+docker pull centos:7
+docker tag centos:7 registry.cn-hangzhou.aliyuncs.com/macroldj/centos:7
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/centos:7
