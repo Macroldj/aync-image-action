eef2e65606032f0585de5ef03d84321db20341b4
Durgin.Li <dejinx@qq.com>
2025-05-13 13:25:53 +0800

    build(sync.sh): update image synchronization commands for Prometheus components

--- sync.sh
+++ sync.sh
@@ -733,15 +733,28 @@ done
 #docker tag rancher/mirrored-library-busybox:1.31.1 registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-library-busybox:1.31.1
 #docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-library-busybox:1.31.1
 
-docker pull mirrored-kube-state-metrics-kube-state-metrics:v2.10.1
-docker tag mirrored-kube-state-metrics-kube-state-metrics:v2.10.1 registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-kube-state-metrics-kube-state-metrics:v2.10.1
-docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-kube-state-metrics-kube-state-metrics:v2.10.1
+#docker pull mirrored-kube-state-metrics-kube-state-metrics:v2.10.1
+#docker tag mirrored-kube-state-metrics-kube-state-metrics:v2.10.1 registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-kube-state-metrics-kube-state-metrics:v2.10.1
+#docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-kube-state-metrics-kube-state-metrics:v2.10.1
 
 
-docker pull rancher/mirrored-grafana-grafana:10.4.9
-docker tag rancher/mirrored-grafana-grafana:10.4.9 registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-grafana-grafana:10.4.9
-docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-grafana-grafana:10.4.9
+#docker pull rancher/mirrored-grafana-grafana:10.4.9
+#docker tag rancher/mirrored-grafana-grafana:10.4.9 registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-grafana-grafana:10.4.9
+#docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-grafana-grafana:10.4.9
 
-docker pull rancher/mirrored-library-nginx:1.24.0-alpine
-docker tag rancher/mirrored-library-nginx:1.24.0-alpine registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-library-nginx:1.24.0-alpine
-docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-library-nginx:1.24.0-alpine
+#docker pull rancher/mirrored-library-nginx:1.24.0-alpine
+#docker tag rancher/mirrored-library-nginx:1.24.0-alpine registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-library-nginx:1.24.0-alpine
+#docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-library-nginx:1.24.0-alpine
+
+docker pull rancher/mirrored-prometheus-operator-prometheus-operator:v0.72.0
+docker tag rancher/mirrored-prometheus-operator-prometheus-operator:v0.72.0 registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-prometheus-operator-prometheus-operator:v0.72.0
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-prometheus-operator-prometheus-operator:v0.72.0
+
+docker pull rancher/mirrored-prometheus-adapter-prometheus-adapter:v0.12.0
+docker tag rancher/mirrored-prometheus-adapter-prometheus-adapter:v0.12.0 registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-prometheus-adapter-prometheus-adapter:v0.12.0
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-prometheus-adapter-prometheus-adapter:v0.12.0
+
+docker pull rancher/mirrored-prometheus-node-exporter:v1.7.0
+docker tag rancher/mirrored-prometheus-node-exporter:v1.7.0 registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-prometheus-node-exporter:v1.7.0
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/mirrored-prometheus-node-exporter:v1.7.0
+
