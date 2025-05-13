d7b4056562d8d0019668e5e903b406744e8f9246
Durgin.Li <dejinx@qq.com>
2025-05-13 11:13:14 +0800

    build(sync.sh): comment out k8s-device-plugin image commands and add metrics-server image synchronization commands

--- sync.sh
+++ sync.sh
@@ -721,6 +721,10 @@ done
 #docker tag dockurr/dnsmasq:2.91 registry.cn-hangzhou.aliyuncs.com/macroldj/dnsmasq:2.91
 #docker push registry.cn-hangzhou.aliyuncs.com/macroldj/dnsmasq:2.91
 
-docker pull nvcr.io/nvidia/k8s-device-plugin:v0.14.0
-docker tag nvcr.io/nvidia/k8s-device-plugin:v0.14.0 registry.cn-hangzhou.aliyuncs.com/macroldj/k8s-device-plugin:v0.14.0
-docker push registry.cn-hangzhou.aliyuncs.com/macroldj/k8s-device-plugin:v0.14.0
+#docker pull nvcr.io/nvidia/k8s-device-plugin:v0.14.0
+#docker tag nvcr.io/nvidia/k8s-device-plugin:v0.14.0 registry.cn-hangzhou.aliyuncs.com/macroldj/k8s-device-plugin:v0.14.0
+#docker push registry.cn-hangzhou.aliyuncs.com/macroldj/k8s-device-plugin:v0.14.0
+
+docker pull registry.k8s.io/metrics-server/metrics-server:v0.7.2
+docker tag registry.k8s.io/metrics-server/metrics-server:v0.7.2 registry.cn-hangzhou.aliyuncs.com/macroldj/metrics-server:v0.7.2
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/metrics-server:v0.7.2
