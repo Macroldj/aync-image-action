482e8e06032e572b1dc1447427dbbea51600214a
Durgin.Li <dejinx@qq.com>
2025-05-14 10:28:56 +0800

    build(sync.sh): comment out guestbook image synchronization and add redis-operator image synchronization commands

--- sync.sh
+++ sync.sh
@@ -770,6 +770,10 @@ done
 #docker tag openkruise/guestbook:v1 registry.cn-hangzhou.aliyuncs.com/macroldj/guestbook:v1
 #docker push registry.cn-hangzhou.aliyuncs.com/macroldj/guestbook:v1
 
-docker pull openkruise/guestbook:v2
-docker tag openkruise/guestbook:v2 registry.cn-hangzhou.aliyuncs.com/macroldj/guestbook:v2
-docker push registry.cn-hangzhou.aliyuncs.com/macroldj/guestbook:v2
+#docker pull openkruise/guestbook:v2
+#docker tag openkruise/guestbook:v2 registry.cn-hangzhou.aliyuncs.com/macroldj/guestbook:v2
+#docker push registry.cn-hangzhou.aliyuncs.com/macroldj/guestbook:v2
+
+docker pull ghcr.io/ot-container-kit/redis-operator/redis-operator:v0.20.2
+docker tag ghcr.io/ot-container-kit/redis-operator/redis-operator:v0.20.2 registry.cn-hangzhou.aliyuncs.com/macroldj/redis-operator:v0.20.2
+docker push registry.cn-hangzhou.aliyuncs.com/macroldj/redis-operator:v0.20.2
